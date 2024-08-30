5a2ba8bad001185ce63e5628742b672856568584
RolembergJunior <junior.45098@gmail.com>
2024-08-30 11:15:47 -0300

    Adjusting countTaskStatus

--- src/app/dashboard/components/CountTaskStatus/index.tsx
+++ src/app/dashboard/components/CountTaskStatus/index.tsx
@@ -1,25 +1,91 @@
+'use client'
 
+import { tasksProps } from "@/app/types/Types";
+import { useFetch } from "@/hooks/useFetch";
 
 export default function CountTaskStatus(){
+    const { data } = useFetch('http://localhost:3000/tarefas');
+
+    if(!data) return; 
+
+    let totalItens = data.length;
+
+    type StatusETarefas = {
+        [status: string]: tasksProps[];
+    };
+
+    function separeStatusInArrays(){
+        const statusTarefas: StatusETarefas = {};
+
+        data?.forEach( item => {
+            const status:string = item.status;
+
+            if( status in statusTarefas ) {
+                statusTarefas[status].push(item);
+            } else {
+                statusTarefas[status] = [item]
+            }
+        });
+
+        return statusTarefas;
+    }
+
+    const statusTasks = separeStatusInArrays();
+
     return(
         <div className="flex flex-col justify-around row-start-1 row-end-2 col-start-1 col-end-2 bg-white dark:bg-[#1e293b] p-4">
-            <h1 className="text-center text-xl">Contadores de status das tasks</h1>
+            <h1 className="text-center text-xl font-semibold">Contador Geral</h1>
             <div className="flex items-center w-full">
-                <div className="bg-red-700 h-10 w-full" />
-                <div className="bg-blue-600 h-10 w-full" />
-                <div className="bg-green-600 h-10 w-full" />
+                <div 
+                    style={{
+                        width: `${((statusTasks['Não iniciado']?.length ? statusTasks['Não iniciado']?.length : 0) / totalItens) * 100}%`
+                    }}    
+                    className="bg-gray-500 h-10" 
+                />
+                <div 
+                    style={{
+                        width: `${((statusTasks['Atrasada']?.length ? statusTasks['Atrasada']?.length : 0) / totalItens) * 100}%`
+                    }}    
+                    className="bg-red-700 h-10" 
+                />
+                <div 
+                    style={{
+                        width: `${((statusTasks['Fazer']?.length ? statusTasks['Fazer']?.length : 0) / totalItens) * 100}%`
+                    }}   
+                    className="bg-blue-500 h-10 w-full" 
+                />
+                <div 
+                    style={{
+                        width: `${((statusTasks['Em andamento']?.length ? statusTasks['Em andamento']?.length : 0) / totalItens) * 100}%`
+                    }}   
+                    className="bg-yellow-500 h-10 w-full" 
+                />
+                <div 
+                    style={{
+                        width: `${((statusTasks['Concluida']?.length ? statusTasks['Concluida']?.length : 0) / totalItens) * 100}%`
+                    }}   
+                    className="bg-green-600 h-10 w-full" 
+                />
             </div>
             <div className="flex justify-around ">
                 <div className="text-center">
-                    <p className="text-xl">20%</p>
+                    <p className="text-xl">{((statusTasks['Não iniciado']?.length ? statusTasks['Não iniciado']?.length : 0) / totalItens) * 100}%</p>
+                    <span className="text-gray-500 font-semibold">Não iniciado</span>
+                </div>
+                <div className="text-center">
+                    <p className="text-xl">{((statusTasks['Atrasada']?.length ? statusTasks['Atrasada']?.length : 0) / totalItens) * 100}%</p>
                     <span className="text-red-700 font-semibold">Atrasadas</span>
                 </div>
                 <div className="text-center">
-                    <p className="text-xl">20%</p>
-                    <span className="text-blue-600 font-semibold">Em andamento</span>
+                    <p className="text-xl">{((statusTasks['Fazer']?.length ? statusTasks['Fazer']?.length : 0) / totalItens) * 100}%</p>
+                    <span className="text-blue-500 font-semibold">Fazer</span>
                 </div>
                 <div className="text-center">
-                    <p className="text-xl">20%</p>
+                    <p className="text-xl">{((statusTasks['Em andamento']?.length ? statusTasks['Em andamento']?.length : 0) / totalItens) * 100}%</p>
+                    <span className="text-yellow-500 font-semibold">Em andamento</span>
+                </div>
+                <div className="text-center">
+                    <p className="text-xl">{((statusTasks['Concluida']?.length ? statusTasks['Concluida']?.length : 0) / totalItens) * 100}%</p>
                     <span className="text-green-600 font-semibold">Concluídas</span>
                 </div>
             </div>
