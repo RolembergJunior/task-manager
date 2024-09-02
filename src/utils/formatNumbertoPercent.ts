8ccc09fa1ade56f99e3df2dd2978445cc3af9be4
RolembergJunior <junior.45098@gmail.com>
2024-09-01 22:31:50 -0300

    Adjusting function formatNumberToPercent

--- src/utils/formatNumbertoPercent.ts
+++ src/utils/formatNumbertoPercent.ts
@@ -1,7 +1,7 @@
 
 
 export function formatNumbertoPercent(value:number, decimals:number){
-    if(!value) return;
+    if(!value) return '0%';
 
     return `${parseFloat(value.toFixed(decimals)) * 100}%`;
 }
